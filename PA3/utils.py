7ce158d816a63557f4f92e2be6754cfac35a0387
Gauthamrr <61095989+Gauthamrr@users.noreply.github.com>
2021-02-07 15:14:10 -0800

    Update utils.py

--- PA3/utils.py
+++ PA3/utils.py
@@ -1,34 +1,23 @@
-def iou(pred, target):
-    """
-        Computes the intersection over union metric
-        pred - Prediction
-        target - Target sample
-    """
+def iou(pred,target):
     ious = []
+    avg_iou = []
     for i in range(len(pred)):
-        for cls in range(n_class):
-            # Complete this function
-            #Gautham: Completed assuming pred and target are tuples with each image in them being for a particular class
-            intersection = numpy.logical_and(pred[i][cls], target[i][cls])# intersection calculation
-            union = numpy.logical_or(pred[i][cls], target[i][cls])#Union calculation
+        for cls in range(n_class): # Go over all classes
+            intersection=float(sum((pred[i][:]==target[i][:])*(target[i][:]==cls)))# per class Intersection
+            union=sum(target[i][:]==cls)+sum(pred[i][:]==cls)-intersection # per class Union
             if union == 0:
                 ious[i].append(float('nan'))  # if there is no ground truth, do not include in evaluation
             else:
-                ious[i].append(float(intersection/union))# Append the calculated IoU to the list ious
-        avg_iou.append(np.mean(np.array(ious[i][0:26])))
+                ious[i].append(intersection/union)# Append the calculated IoU to the list ious
+        avg_iou[i] = np.mean(np.array(ious[i][0:26])) #discarding class=26
     return ious
 
-
 def pixel_acc(pred, target):
-    """
-        Computes the pixel accuracy for a given prediction
-        Target
-    """
-    #Complete this function
     accs = []
     for i in range(len(pred)):
-        h, w = target[i][0].shape #shape of prediction in each image per class
+        h, w = target[i].shape
         for cls in range(n_class):
+            #shape of prediction in each image per class
             if cls!=26:
                 accs[i].append(float(sum(pred[i][cls] == target[i][cls])/(h*w)))
         avg_acc = np.mean(np.array(accs[i][:]))   
