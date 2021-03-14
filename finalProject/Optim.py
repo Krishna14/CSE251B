fe8baafdadc98d33ded65dc50942d837ebe00acb
Krishna14 <krishna1994sree@gmail.com>
2021-03-13 19:33:23 -0800

    Added code for Transformer model

---
+++ finalProject/Optim.py
@@ -0,0 +1,68 @@
+import torch
+import numpy as np
+# code from AllenNLP
+
+class CosineWithRestarts(torch.optim.lr_scheduler._LRScheduler):
+    """
+    Cosine annealing with restarts.
+    Parameters
+    ----------
+    optimizer : torch.optim.Optimizer
+    T_max : int
+        The maximum number of iterations within the first cycle.
+    eta_min : float, optional (default: 0)
+        The minimum learning rate.
+    last_epoch : int, optional (default: -1)
+        The index of the last epoch.
+    """
+
+    def __init__(self,
+                 optimizer: torch.optim.Optimizer,
+                 T_max,
+                 eta_min = 0.,
+                 last_epoch = -1,
+                 factor = 1.):
+        # pylint: disable=invalid-name
+        self.T_max = T_max
+        self.eta_min = eta_min
+        self.factor = factor
+        self._last_restart = 0
+        self._cycle_counter = 0
+        self._cycle_factor = 1.
+        self._updated_cycle_len = T_max
+        self._initialized = False
+        super(CosineWithRestarts, self).__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        """Get updated learning rate."""
+        # HACK: We need to check if this is the first time get_lr() was called, since
+        # we want to start with step = 0, but _LRScheduler calls get_lr with
+        # last_epoch + 1 when initialized.
+        if not self._initialized:
+            self._initialized = True
+            return self.base_lrs
+
+        step = self.last_epoch + 1
+        self._cycle_counter = step - self._last_restart
+
+        lrs = [
+            (
+                self.eta_min + ((lr - self.eta_min) / 2) *
+                (
+                    np.cos(
+                        np.pi *
+                        ((self._cycle_counter) % self._updated_cycle_len) /
+                        self._updated_cycle_len
+                    ) + 1
+                )
+            ) for lr in self.base_lrs
+        ]
+
+        if self._cycle_counter % self._updated_cycle_len == 0:
+            # Adjust the cycle length.
+            self._cycle_factor *= self.factor
+            self._cycle_counter = 0
+            self._updated_cycle_len = int(self._cycle_factor * self.T_max)
+            self._last_restart = step
+
+        return lrs
